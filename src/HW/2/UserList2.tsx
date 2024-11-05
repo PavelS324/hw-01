0118904d2b6b7d98899ebaa9b8e489e540ddd002
Pavel Sasim <pavels324@gmail.com>
2024-11-05 11:04:41 +0300

    Made therd home work

--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -2,8 +2,8 @@ import {CurrentUser} from './CurrentUser';
 import {UsersObjectType} from './HW2';
 
 type UserList2PropsType = {
-	users: any // пропиши типизацию
-	filterUsers: any // пропиши типизацию
+	users: UsersObjectType // пропиши типизацию
+	filterUsers: () => void // пропиши типизацию
 };
 
 export const UserList2 = (props: UserList2PropsType) => {
@@ -11,13 +11,17 @@ export const UserList2 = (props: UserList2PropsType) => {
 		<div id={'hw02-users'}>
 			<h2>User List:</h2>
 
-			{/*<button id={'hw02-filter-button'} onClick={() => props.filterUsers}>SHOW ME FRIENDS FROM LA</button>*/}
+			<button id={'hw02-filter-button'} onClick={() => props.filterUsers()}>SHOW ME FRIENDS FROM LA</button>
 
-			{/*<ul>*/}
-			{/*  {props.users.myFriends.map((user) => (*/}
-			{/*    <ВЫЗЫВАЕМ КОМПОНЕНТУ CurrentUser />*/}
-			{/*  ))}*/}
-			{/*</ul>*/}
+			<ul>
+				{props.users.myFriends.map((user, idNum) => {
+					return (<li key={idNum} id={`hw02-user-${user.id}`}>
+						<strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+						{user.address.street}, {user.address.city}
+					</li>)
+					// <ВЫЗЫВАЕМ КОМПОНЕНТУ CurrentUser />
+				})}
+			</ul>
 		</div>
 	);
 };
